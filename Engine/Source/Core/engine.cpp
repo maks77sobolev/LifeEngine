7c940ba8daa89e1daa07a070aa00f9d2f60d9505
maks77sobolev <maks77.sobolev@live.com>
2026-01-31 08:44:10 +0200

    first save

---
+++ Engine/Source/Core/engine.cpp
@@ -0,0 +1,7 @@
+#include "engine.h"
+#include <iostream>
+using namespace LifeExe;
+Engine::Engine()
+{
+    std::cout << "Initializing Life Exe Engine....." << std::endl;
+}
---
+++ Engine/Source/main.cpp
@@ -0,0 +1,7 @@
+#include "Core/engine.h"
+#include "stdlib.h"
+int main() {
+    const LifeExe::Engine engine;
+    return EXIT_SUCCESS;
+
+}
